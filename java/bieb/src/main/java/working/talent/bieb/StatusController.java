cc93dbc9b43e9c3b618eb6911bb72251afd60942
Tom <79252861+tinderboxmedia@users.noreply.github.com>
2022-03-02 16:46:26 +0100

    creation of account and address

---
+++ java/bieb/src/main/java/working/talent/bieb/StatusController.java
@@ -0,0 +1,16 @@
+package working.talent.bieb;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+	
+	@RequestMapping(value = "/status")
+	public Date status() {
+		return new Date();
+	}
+	
+}
