f852949baf0f4e4f9c332dbaa7fa211fa74a6216
Tom <79252861+tinderboxmedia@users.noreply.github.com>
2022-03-02 12:08:25 +0100

    added java files and instructions

---
+++ java/bieb/src/main/java/working/talent/bieb/BiebApplication.java
@@ -0,0 +1,13 @@
+package working.talent.bieb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiebApplication.class, args);
+	}
+
+}
